8c9fe8ae5d98ae4126106d72f49cd1bcd7c1e6a7
Noah Kamara <noah.kamara@uni-konstanz.de>
2023-05-12 23:07:01 +0200

    feat(auth): Implement AuthController - register - login - logout - get auth status - authorization middleware

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,18 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { UserController } from './controllers/UserController';
-import { IDUser } from './models/user';
-import { User } from './models/user';
+import { IDUser, User } from './models/user';
 import { Session } from './models/session';
-
 import bcrypt from 'bcrypt';
 import uid from 'uid-safe';
-
-// import { User } from './models/user';
-// import { Session } from './models/session';
-// import uid from 'uid-safe';
-// import bcrypt from 'bcrypt';
-
 
 class MockUserController implements UserController {
     users: IDUser[] = [
@@ -23,8 +15,8 @@ class MockUserController implements UserController {
     sessions: Session[] = [];
 
     createUser(user: User): IDUser {
-        const idUser: IDUser = { 
-            id: this.users.length + 1, 
+        const idUser: IDUser = {
+            id: this.users.length + 1,
             ...user
         };
 
@@ -32,35 +24,88 @@ class MockUserController implements UserController {
         return idUser;
     }
 
-    createSession(user: IDUser): string {
+    getUserByEmail(email: string): IDUser {
+        return this.users.find(u => u.email === email);
+    }
+
+    getUserById(id: number): IDUser {
+        return this.users.find(u => u.id === id);
+    }
+
+    createSession(user: IDUser): Session {
         const sessionId = uid.sync(24);
         const session: Session = { sessionId: sessionId, userId: user.id };
 
         // add to active sessions
         this.sessions.push(session);
 
-        return session.sessionId;
+        return session;
+    }
+
+    getSession(sessionId: string): Session {
+        return this.sessions.find(s => s.sessionId === sessionId);
+    }
+
+    delSession(sessionId: string): void {
+        const index = this.sessions.findIndex(s => s.sessionId === sessionId);
+        if (!index) return;
+        this.sessions.splice(index, 1);
     }
 }
 
 export class AuthController {
-    public userController: UserController;
+    private userController: UserController;
+
+    private salt: number | string = 10;
     
-    constructor(userController: UserController = new MockUserController()) {
+    /**
+    * Brief description of the constructor here.
+    *
+    * @constructor
+    * @param {UserController} userController - Brief description of the parameter here.
+    */
+    constructor(userController: UserController = new MockUserController(), salt: number | string = 10) {
         this.userController = userController;
+        this.salt = salt;
     }
 
     /** 
-    * Brief description of the function here.
-    * @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
-    * @param {ParamDataTypeHere} parameterNameHere - Brief description of the parameter here. Note: For other notations of data types, please refer to JSDocs: DataTypes command.
-    * @return {ReturnValueDataTypeHere} Brief description of the returning value here.
+    * Authorize a user by session cookie
     */
-    register = (request: Request, response: Response): void => {
-        // hash pwd
-        // request.body.json
+    authorize(request: Request, response: Response, next: NextFunction): void {
+        // get session id from cookies
+        const sessionId = request.cookies.sessionId;
+        if (!sessionId) {
+            next();
+            return;
+        }
+
+        // get session by id
+        const sess = this.userController.getSession(sessionId);
+        if (!sess) {
+            next();
+            return;
+        }
+
+        // get user by id
+        const user = this.userController.getUserById(sess.userId);
+        if (!user) {
+            next();
+            return;
+        }
+
+        response.locals.session = sess;
+        response.locals.user = user;
+
+        return next();
+    }
+
+    /** 
+    * Create a new user
+    */
+    createUser(request: Request, response: Response): void {
         const password = request.body.password;
-        const passwordHash = bcrypt.hashSync(password, 10);
+        const passwordHash = bcrypt.hashSync(password, this.salt);
         const firstName = request.body.firstName;
         const lastName = request.body.lastName;
         const email = request.body.email;
@@ -74,11 +119,72 @@ export class AuthController {
         });
 
         // create session
-        const sessionId = this.userController.createSession(user);
+        const session = this.userController.createSession(user);
 
         response.status(200);
-        response.cookie('sessionId', sessionId, { httpOnly: true });
+        response.cookie('sessionId', session.sessionId, { httpOnly: true });
         response.send({ code: 200, message: 'Login successful' });
         return;
-    };
+    }
+
+    /** 
+     *  returns true if the user is authorized, otherwise false
+    */
+    getAuth(request: Request, response: Response): void {
+        if (!response.locals.session) {
+            response.status(200);
+            response.send(false);
+            return;
+        }
+
+        response.status(200);
+        response.send(true);
+        return;
+    }
+
+    /** 
+    * Login an existing user 
+    */
+    login(request: Request, response: Response): void {
+        const email: string = request.body.email;
+        const password: string = request.body.password;
+
+        // retrieve user
+        const user = this.userController.getUserByEmail(email);
+
+        if (!user) {
+            response.status(401);
+            response.send({ code: 401, message: 'E-Mail address not found' });
+            return;
+        }
+
+        // check password
+        if (!bcrypt.compareSync(password, user.password)) {
+            response.status(401);
+            response.send({ code: 401, message: 'Wrong password' });
+            return;
+        }
+
+        // create session token
+        const session = this.userController.createSession(user);
+
+        response.status(200);
+        response.cookie('sessionId', session.sessionId, { httpOnly: true });
+        response.send({ code: 200, message: 'Login successful' });
+        return;
+    }
+
+    /** 
+    * logout a currently logged in user
+    */
+    logout(request: Request, response: Response): void {
+        const sessionId = request.cookies.sessionId;
+
+        if (sessionId) {
+            response.send({ code: 404, message: 'Logout failure: No Session with that ID' });
+            return;
+        }
+
+        response.send({ code: 200, message: 'Logout successful' });
+    }
 }
--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 
 import { User, IDUser } from '../models/user';
-
+import { Session } from '../models/session';
 /**
 * Interface for Users
 *
@@ -9,14 +9,35 @@ import { User, IDUser } from '../models/user';
 export interface UserController {
     /** 
     * creates a user in the database and returns it
-    * @param {User} a user without an id
+    * @param {User} user without an id
     * @return {IDUser} a new user with the data from the function parameters
     */
     createUser(user: User): IDUser
 
+    /** 
+    * returns the user for the email, if no user exists, return nothing
+    */
+    getUserByEmail(email: string): IDUser
 
     /** 
-    * creates a new session for a user, _if_ they exist in the database
+    * returns the user for the id, if no user exists, return nothing
     */
-    createSession(user: IDUser): string
+    getUserById(id: number): IDUser
+
+    /** 
+    * creates a new session for a user
+    */
+    createSession(user: IDUser): Session
+
+    /** 
+    * returns a session from the session id, 
+    * @return Session if it exists
+    * @return nothing if there exists no session with this id
+    */
+    getSession(sessionId: string): Session
+
+    /** 
+    * deletes the session 
+    */
+    delSession(sessionId: string): void
 }
