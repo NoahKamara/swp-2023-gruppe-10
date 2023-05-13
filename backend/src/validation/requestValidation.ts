3fe29dd42caa7cdff4ff704c955448447cc1aa9c
Noah Kamara <mail@noahkamara.com>
2023-05-13 21:27:34 +0200

    doc(auth): Add documentation

--- backend/src/validation/requestValidation.ts
+++ backend/src/validation/requestValidation.ts
@@ -1,16 +1,27 @@
 import { ZodSchema, ZodError } from 'zod';
 import { Request, Response } from 'express';
 
-export const validateBody = <T>(req: Request, res: Response, schema: ZodSchema<T>): T | void => {
-    try {
-      const validatedData = schema.parse(req.body);
-      return validatedData;
-    } catch (err) {
-      if (err instanceof ZodError) {
-        res.status(400).json({ errors: err.flatten() });
-      } else {
-        res.sendStatus(500);
-      }
+/**
+ * validates the request body against the provided schema
+ *
+ * @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
+ * @param {Request} req - express request
+ * @param {Response} res - express response
+ * @return {T | void} if the schema matches, returns the resulting object, otherwise return nothing and send error to client
+ */
+export const validateBody = <T>(
+  req: Request,
+  res: Response,
+  schema: ZodSchema<T>
+): T | void => {
+  try {
+    const validatedData = schema.parse(req.body);
+    return validatedData;
+  } catch (err) {
+    if (err instanceof ZodError) {
+      res.status(400).json({ errors: err.flatten() });
+    } else {
+      res.sendStatus(500);
     }
-  };
-  
+  }
+};
