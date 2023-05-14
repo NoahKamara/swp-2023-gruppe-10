0e52139a5a42edf66008b4dfb67d0bf9b0987679
Noah Kamara <noah.kamara@uni-konstanz.de>
2023-05-14 14:34:26 +0200

    test(auth): change reporter - remove jasmine-spec-reporter - add jasmine-console-reporter - modify 'test' script to use new reporter

--- backend/specs/auth.spec.ts
+++ backend/specs/auth.spec.ts
@@ -6,27 +6,6 @@
 
 import request from 'supertest';
 import app from '../src/app';
-import { DisplayProcessor, SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
-import SuiteInfo = jasmine.SuiteInfo
-import { attachCookies } from 'superagent';
-
-class CustomProcessor extends DisplayProcessor {
-  public displayJasmineStarted(info: SuiteInfo, log: string): string {
-    return `TypeScript ${log}`;
-  }
-}
-
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(
-  new SpecReporter({
-    spec: {
-      displayStacktrace: StacktraceOption.NONE,
-    },
-    customProcessors: [CustomProcessor],
-  })
-);
-
-
 
 
 describe('POST /api/user (User Registration)', () => {
