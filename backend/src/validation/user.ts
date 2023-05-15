e2b12a13b4a2fcda5dd6ea1423e891ff50e2f02a
Noah Kamara <noah.kamara@uni-konstanz.de>
2023-05-15 13:53:49 +0200

    fix(auth): refactor z validation - schemas now automatically generated correctly - email & minlength password specified as modifiers

--- backend/src/validation/user.ts
+++ backend/src/validation/user.ts
@@ -2,13 +2,11 @@
 import { z } from 'zod';
 
 export const userLoginInfoSchema = z.object({
-  email: z.string(),
-  password: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
 });
 
 export const userInfoSchema = userLoginInfoSchema.extend({
-  email: z.string().email(),
-  password: z.string().min(8),
   firstName: z.string(),
   lastName: z.string(),
 });
