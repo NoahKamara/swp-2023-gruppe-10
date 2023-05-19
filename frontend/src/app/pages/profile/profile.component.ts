7549b09ca3263c2c36ff6ca6a0867525c3898909
Noah Kamara <noah.kamara@uni-konstanz.de>
2023-05-19 17:30:19 +0200

    fix(auth)!: Refactored Loginservied

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginService } from 'src/app/services/login.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   templateUrl: './profile.component.html',
@@ -8,7 +8,7 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class ProfileComponent {
 
-  constructor(private router: Router, private loginService: LoginService) {}
+  constructor(private router: Router, private loginService: AuthService) {}
   logout(): void {
     this.loginService.loggedIn = false;
     this.router.navigateByUrl('/login');
