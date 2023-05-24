4bf326f6cfd22e33428a008854475386ee535707
niklas.groene <niklas.groene@uni-konstanz.de>
2023-05-24 10:33:13 +0200

    Persöhnliche Informationen

---
+++ frontend/src/app/components/change-pw/change-pw.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './change-pw.component.html',
+  styleUrls: ['./change-pw.component.css']
+})
+export class ChangePwComponent {
+
+  public oldpw = '';
+  public newpw1 = '';
+  public newpw2 = '';
+
+  save(): void {
+    // Bitte Logik einfügen
+  }
+
+  constructor(prof)
+  
+
+}
