20cc0fba479138815ecbb2b22c0dc17f0b138954
Marius Berner <marius.berner@uni.kn>
2023-07-12 12:54:07 +0200

    minor changes in event-list-item

--- frontend/src/app/components/event-list-item/event-list-item.component.ts
+++ frontend/src/app/components/event-list-item/event-list-item.component.ts
@@ -7,6 +7,17 @@ import { EventListItem } from 'softwareproject-common';
   styleUrls: ['./event-list-item.component.css']
 })
 export class EventListItemComponent {
+  public isFavorite = false;
+  
   @Input()
   public item!: EventListItem;
+
+  didClickFavButton(): void {
+    if(this.isFavorite === false){
+      this.isFavorite = true;
+    }
+    else{
+      this.isFavorite = false;
+    }
+  }
 }
